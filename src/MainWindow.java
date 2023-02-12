5f264642cf7ea20626441ad04782ac5679195a04
Matúš <matussusky@gmail.com>
2023-02-12 18:32:39 +0100

    Version 0.1.0 -> 0.1.2

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -26,7 +26,7 @@ import java.util.Objects;
 
 public class MainWindow extends Application {
     public static final String AUTHOR = "Matúš Suský";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.2";
     private final Hyperlink icons = new Hyperlink("-Icons");
     private final Hyperlink darkThemeC = new Hyperlink("-Dark theme colors");
     private final Hyperlink lightThemeC = new Hyperlink("-Light theme colors");
